83ed624c5e819777b9c2a320f3d144db7cc8f8ee
Wingelo <cedric.gamrat@gmail.com>
2025-07-10 01:43:13 +0200

    Wingelo patch 2 (#2)
    * Create main.yml

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -30,7 +30,6 @@ module.exports = function (config) {
       reporters: [
         { type: 'html' },
         { type: 'text-summary' }
-        { type: 'lcovonly' }
       ]
     },
     reporters: ['progress', 'kjhtml'],
